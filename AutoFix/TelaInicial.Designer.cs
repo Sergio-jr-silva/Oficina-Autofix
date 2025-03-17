c1aed29e5e0191ccaae41f82333baace4b67ab84
Sergio_jrsilva <juniosergio034@gmail.com>
2025-03-17 20:47:41 -0300

    Adicionar arquivos de projeto.

---
+++ AutoFix/TelaInicial.Designer.cs
@@ -0,0 +1,234 @@
+﻿namespace AutoFix
+{
+    partial class TelaInicial
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TelaInicial));
+            pictureBox1 = new PictureBox();
+            label1 = new Label();
+            pictureBox2 = new PictureBox();
+            label2 = new Label();
+            pictureBox3 = new PictureBox();
+            label3 = new Label();
+            pictureBox4 = new PictureBox();
+            label4 = new Label();
+            label5 = new Label();
+            pictureBox5 = new PictureBox();
+            label6 = new Label();
+            pictureBox6 = new PictureBox();
+            label7 = new Label();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox6).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources.user;
+            pictureBox1.Location = new Point(56, 47);
+            pictureBox1.Margin = new Padding(4, 3, 4, 3);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(139, 85);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Arial Rounded MT Bold", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.Location = new Point(56, 151);
+            label1.Name = "label1";
+            label1.Size = new Size(150, 20);
+            label1.TabIndex = 1;
+            label1.Text = "adicionar Cliente";
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Image = Properties.Resources.consulta;
+            pictureBox2.Location = new Point(250, 47);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(153, 85);
+            pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox2.TabIndex = 2;
+            pictureBox2.TabStop = false;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Arial Rounded MT Bold", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.Location = new Point(251, 151);
+            label2.Name = "label2";
+            label2.Size = new Size(152, 20);
+            label2.TabIndex = 3;
+            label2.Text = "Consultar Cliente";
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.Image = Properties.Resources.orcamento;
+            pictureBox3.Location = new Point(452, 49);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(141, 83);
+            pictureBox3.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox3.TabIndex = 4;
+            pictureBox3.TabStop = false;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(472, 151);
+            label3.Name = "label3";
+            label3.Size = new Size(109, 21);
+            label3.TabIndex = 5;
+            label3.Text = "Orçamento";
+            // 
+            // pictureBox4
+            // 
+            pictureBox4.Image = Properties.Resources.car;
+            pictureBox4.Location = new Point(56, 231);
+            pictureBox4.Name = "pictureBox4";
+            pictureBox4.Size = new Size(969, 253);
+            pictureBox4.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox4.TabIndex = 6;
+            pictureBox4.TabStop = false;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Bernard MT Condensed", 25.8000011F, FontStyle.Italic, GraphicsUnit.Point, 0);
+            label4.ForeColor = Color.Red;
+            label4.Location = new Point(287, 420);
+            label4.Name = "label4";
+            label4.Size = new Size(159, 52);
+            label4.TabIndex = 7;
+            label4.Text = "AUTOFIX";
+            label4.Click += label4_Click;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Bernard MT Condensed", 25.8000011F, FontStyle.Italic, GraphicsUnit.Point, 0);
+            label5.Location = new Point(452, 420);
+            label5.Name = "label5";
+            label5.Size = new Size(341, 52);
+            label5.TabIndex = 8;
+            label5.Text = "Oficina Automotiva";
+            // 
+            // pictureBox5
+            // 
+            pictureBox5.Image = Properties.Resources.agenda;
+            pictureBox5.Location = new Point(641, 49);
+            pictureBox5.Name = "pictureBox5";
+            pictureBox5.Size = new Size(152, 83);
+            pictureBox5.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox5.TabIndex = 9;
+            pictureBox5.TabStop = false;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(625, 151);
+            label6.Name = "label6";
+            label6.Size = new Size(190, 21);
+            label6.TabIndex = 10;
+            label6.Text = "Agenda De Serviços";
+            // 
+            // pictureBox6
+            // 
+            pictureBox6.Image = Properties.Resources.estoque;
+            pictureBox6.Location = new Point(860, 49);
+            pictureBox6.Name = "pictureBox6";
+            pictureBox6.Size = new Size(135, 83);
+            pictureBox6.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox6.TabIndex = 11;
+            pictureBox6.TabStop = false;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(845, 151);
+            label7.Name = "label7";
+            label7.Size = new Size(165, 21);
+            label7.TabIndex = 12;
+            label7.Text = "Consulta Estoque";
+            // 
+            // TelaInicial
+            // 
+            AutoScaleDimensions = new SizeF(11F, 21F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ButtonFace;
+            ClientSize = new Size(1089, 542);
+            Controls.Add(label7);
+            Controls.Add(pictureBox6);
+            Controls.Add(label6);
+            Controls.Add(pictureBox5);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(pictureBox4);
+            Controls.Add(label3);
+            Controls.Add(pictureBox3);
+            Controls.Add(label2);
+            Controls.Add(pictureBox2);
+            Controls.Add(label1);
+            Controls.Add(pictureBox1);
+            Font = new Font("Arial Rounded MT Bold", 10.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            ForeColor = Color.Navy;
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Margin = new Padding(4, 3, 4, 3);
+            Name = "TelaInicial";
+            Text = "AutoFix Oficina Automotiva";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox6).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private PictureBox pictureBox1;
+        private Label label1;
+        private PictureBox pictureBox2;
+        private Label label2;
+        private PictureBox pictureBox3;
+        private Label label3;
+        private PictureBox pictureBox4;
+        private Label label4;
+        private Label label5;
+        private PictureBox pictureBox5;
+        private Label label6;
+        private PictureBox pictureBox6;
+        private Label label7;
+    }
+}
---
+++ AutoFix/TelaInicial.cs
@@ -0,0 +1,15 @@
+namespace AutoFix
+{
+    public partial class TelaInicial : Form
+    {
+        public TelaInicial()
+        {
+            InitializeComponent();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
